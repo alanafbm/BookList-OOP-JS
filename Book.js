8b10b3c35b476fdfc82da3105cca49de4d470e3a
Alana Moraes <alanaaaa@hotmail.com>
2022-06-12 17:34:42 -0400

    BookList - Add Class Book

---
+++ Book.js
@@ -0,0 +1,8 @@
+export default class Book {
+    constructor(title, author, description){
+        this._title = title;
+        this._author = author;
+        this._description = description;
+    }
+
+}
