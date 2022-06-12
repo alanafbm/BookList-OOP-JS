fe0e5edc831d689d8ff792f4dd4b0002cbef391f
Alana Moraes <alanaaaa@hotmail.com>
2022-06-12 17:35:30 -0400

    BookList - Add Class LocalStorage

---
+++ LocalStore.js
@@ -0,0 +1,47 @@
+import UI from "./UI.js"
+export default class Store {
+    static getBooks(){
+        let books;
+        if (localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+
+    // display in the UI
+
+    static displayBooks(){
+        const books = Store.getBooks();
+
+        books.forEach(function(book){
+            const ui = new UI;
+
+            // Add book to UI
+            ui.addBookToList(book);
+        })
+
+    }
+    static addBook(book){
+        const books = Store.getBooks();
+
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books));
+
+    }
+    static removeBook(description){
+        const books = Store.getBooks();
+        console.log(books);
+        books.forEach(function(book, index){
+            console.log(book?.description);
+        if (book._description === description) {
+            books.splice(index, 1);
+        }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+
+    }
+}
